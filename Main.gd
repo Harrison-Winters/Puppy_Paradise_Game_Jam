a94c9498cad54d71952b96729fa8ab99a6e964ef
Harrison-Winters <winters23@up.edu>
2022-11-04 21:15:08 -0700

    Random Puppies on Screen

--- Main.gd
+++ Main.gd
@@ -3,7 +3,8 @@ extends Node2D
 var rng = RandomNumberGenerator.new()
 var store_cute = 300
 var puppy_texture = preload("res://art/dog_realistic.png")
-
+var puppy_texture2 = preload("res://art/puppy_realistic.png")
+var puppy_texture3 = preload("res://art/large_puppy.png")
 
 func _ready():
 	rng.randomize()
@@ -28,11 +29,21 @@ func _process(delta):
 	
 func _addPuppy():
 	var visDog3 = Sprite.new()
-	visDog3.texture = puppy_texture
+	var pup_t
 	
 	var rand_x = rng.randi_range(500, 1050)
 	var rand_y = rng.randi_range(0, 600)
+	var rand_p = rng.randi_range(0, 2)
 	
+	match rand_p:
+		0:
+			pup_t = puppy_texture
+		1:
+			pup_t = puppy_texture2
+		2:
+			pup_t = puppy_texture3
+	
+	visDog3.texture = pup_t
 	
 	visDog3.set_position(Vector2(rand_x, rand_y))
 	visDog3.scale.x = 0.063
