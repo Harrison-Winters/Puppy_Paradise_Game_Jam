2c433b770e5903da9260e9f998c48efa4a3ba626
kellye24 <103087185+kellye24@users.noreply.github.com>
2022-11-05 15:45:42 -0700

    various changes
    various changes to sounds, display, etc

--- CatTimerLabel.gd
+++ CatTimerLabel.gd
@@ -1,8 +1,11 @@
 extends Label
 
+var meow_audio = preload("res://audio/meow.mp3")
 
 func _process(delta):
 	set_text("Cat Timer: \n" + "%d:%02d" % [floor($CatTimer.time_left / 60), int($CatTimer.time_left) % 60])
+	if int($CatTimer.time_left) % 60 == 15:
+		$Meow.play()
 
 
 
